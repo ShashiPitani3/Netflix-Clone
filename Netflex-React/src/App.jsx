53270c93faf972d7b02dc8e6ab21131061d34cac
Shashi <shashiumasripitani@gmail.com>
2025-03-03 08:46:19 +0530

    initializing netflix clone

---
+++ Netflex-React/src/App.jsx
@@ -0,0 +1,23 @@
+import {React} from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+
+
+const App = () => {
+  
+    return (
+    <div>
+      <Navbar/>
+
+      
+    </div>
+  )
+}
+
+
+
+ 
+    
+
+
+export default App;
